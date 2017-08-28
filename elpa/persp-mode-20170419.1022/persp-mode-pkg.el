7b1cc922f32854f429f058bbb84c17567ec1ad93
Jason S Graham <jgraham20@gmail.com>
2017-08-28 08:43:34 -0500

    Add Packages

---
+++ elpa/persp-mode-20170419.1022/persp-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "persp-mode" "20170419.1022" "windows/buffers sets shared among frames + save/load." 'nil :commit "3e45de29ea6ef0431df65db18f4a73615c409dfb" :url "https://github.com/Bad-ptr/persp-mode.el" :keywords '("perspectives" "session" "workspace" "persistence" "windows" "buffers" "convenience"))
