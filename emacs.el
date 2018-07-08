27e32992bc4da5c3770375b604e779346e623516
bigleafio <jgraham20@gmail.com>
2018-07-07 22:27:47 -0500

    Add features

--- emacs.el
+++ emacs.el
@@ -4,12 +4,13 @@
 ;;----------------------------------------------------------------------------
 ;; Adjust garbage collection thresholds during startup, and thereafter
 ;;----------------------------------------------------------------------------
+(setq byte-compile-warnings '(not free-vars unresolved noruntime lexical make-local))
 
-;;(let ((normal-gc-cons-threshold (* 20 1024 1024))
-  ;;    (init-gc-cons-threshold (* 128 1024 1024)))
-  ;;(setq gc-cons-threshold init-gc-cons-threshold)
-  ;;(add-hook 'after-init-hook
-  ;;          (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'after-init-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 ;; Package configs
 (require 'package)
@@ -26,28 +27,48 @@
 (require 'use-package)
 
 (scroll-bar-mode -1)
-(tool-bar-mode   -1)
-(tooltip-mode    -1)
-(menu-bar-mode   1)
+ (tool-bar-mode   -1)
+ (tooltip-mode    -1)
+ (menu-bar-mode   1)
+ (desktop-save-mode 1)
 
-(add-to-list 'default-frame-alist '(font . "PragmataPro"))
-;;(add-to-list 'default-frame-alist '(height . 24))
-;;(add-to-list 'default-frame-alist '(width . 80))
+ (set-default-font "PragmataPro 14")
+ (add-to-list 'default-frame-alist '(font . "PragmataPro-14:spacing=100")) 
+ (add-to-list 'default-frame-alist '(height . 48))
+ (add-to-list 'default-frame-alist '(width . 160))
 
-(use-package eyebrowse
+ ;Set up the Fringe
+ (define-fringe-bitmap 'tilde [64 168 16] nil nil 'center)
+ (set-fringe-bitmap-face 'tilde 'fringe)  
+
+ (use-package eyebrowse
+  :ensure t
+  :config 
+   (eyebrowse-mode t))
+
+ (use-package shackle
+  :ensure t 
+  :init
+    (setq shackle-rules '((compilation-mode :noselect t))
+	  shackle-default-rule '(:select t))
+    (setq helm-display-function 'pop-to-buffer) ; make helm play nice
+    (setq shackle-rules '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :size 0.4)))
+  :config (shackle-mode)
+ )
+
+(use-package imenu-list
  :ensure t
- :config 
-  (eyebrowse-mode t))
+ :demand t
+ :config
+ (setq imenu-list-focus-after-activation t)
+ (general-define-key :keymaps 'imenu-list-major-mode-map
+                     :states '(normal)
+                     "|" 'imenu-list-minor-mode
+                     "RET" 'imenu-list-goto-entry
+                     "i" 'imenu-list-goto-entry
+                     "q" 'imenu-list-quit-window)
 
-(use-package shackle
- :ensure t 
- :init
-   (setq shackle-rules '((compilation-mode :noselect t))
-         shackle-default-rule '(:select t))
-   (setq helm-display-function 'pop-to-buffer) ; make helm play nice
-   (setq shackle-rules '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :size 0.4)))
- :config (shackle-mode)
-)
+ )
 
 ;; Vim mode
 (use-package evil
@@ -63,10 +84,89 @@
   (evil-escape-mode 1))
 
 ;; Theme
-(use-package doom-themes
+
+(use-package challenger-deep-theme
   :ensure t
+  :init 
+    (load-theme 'challenger-deep t))
+;(use-package doom-themes
+;  :ensure t
+;  :init
+;    ;; Global settings (defaults)
+;    (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      ;     doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;  :config
+;    (load-theme 'doom-tomorrow-night t)
+;    (doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+;    (doom-themes-org-config)) 
+
+ ;(use-package smart-mode-line-powerline-theme
+ ;  :ensure t) 
+
+ (use-package smart-mode-line
+  :ensure t
+  :init
+   (setq sml/theme 'respectful)
+   (setq sml/no-confirm-load-theme t)
+   (sml/setup)
   :config
-  (load-theme 'doom-one t))
+    (setq sml/shorten-directory t
+          sml/shorten-modes t)
+    (add-to-list 'sml/replacer-regexp-list '("^~/Notes/" ":org:"))
+    (add-to-list 'sml/replacer-regexp-list '("^~/Dropbox/" ":DB:")))
+
+(use-package ivy
+  :ensure t
+  :demand t
+  :config
+  ;; regex order
+  (setq ivy-re-builders-alist '((t . ivy--regex-ignore-order)))
+  (define-key ivy-mode-map [escape] 'minibuffer-keyboard-quit)
+  (define-key ivy-minibuffer-map (kbd "C-i") 'ivy-call)
+  (define-key ivy-minibuffer-map (kbd "C-o") 'ivy-occur)
+(general-define-key :keymaps '(ivy-occur-grep-mode-map)
+		    :states '(normal)
+		    "q" 'evil-delete-buffer)
+  (defvar pop-target-window)
+  (make-variable-buffer-local 'pop-target-window)
+  (advice-add 'compilation-goto-locus :around #'my-around-compilation-goto-locus)
+  (defun my-around-compilation-goto-locus (orig-func &rest args)
+    (advice-add 'pop-to-buffer :override #'my-pop-to-buffer)
+    (apply orig-func args))
+  (defun my-pop-to-buffer (buffer &optional action norecord)
+    (advice-remove 'pop-to-buffer #'my-pop-to-buffer)
+    (let ((from-buffer (current-buffer))
+	  (reused-window (display-buffer-reuse-window buffer nil)))
+      (cond (reused-window
+	     (select-window reused-window norecord))
+	    ((and (bound-and-true-p pop-target-window)
+		  (window-live-p pop-target-window))
+	     (window--display-buffer buffer pop-target-window 'reuse)
+	     (select-window pop-target-window norecord))
+	    (t
+	     (pop-to-buffer buffer action norecord)
+	     (with-current-buffer from-buffer
+	       (setq-local pop-target-window (selected-window)))))))
+  (ivy-mode t))
+(use-package counsel
+  :ensure t
+  :demand t
+  :config)
+(use-package swiper
+  :ensure t
+  :demand t
+  :config
+  (ivy-mode t))
+(use-package avy
+  :ensure t
+  :demand t
+  :config
+  (defun avy-line-saving-column ()
+    (interactive)
+    (let ((col (current-column)))
+      (avy-goto-line)
+      (move-to-column col)))
+  )
 
 (use-package helm
   :ensure t
@@ -115,6 +215,15 @@
         ranger-show-dotfiles nil
         ranger-show-literal nil))
 
+;;; Magit
+(use-package magit
+  :ensure t)
+
+(use-package diff-hl
+  :ensure t
+  :config
+    (diff-hl-mode))
+
 ;; Which Key
 (use-package which-key
   :ensure t
@@ -135,14 +244,22 @@
   "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
   "SPC" '(helm-M-x :which-key "M-x")
   ;;"pf"  '(helm-find-files :which-key "find files")
+  ;; Magit
+  "g" '(:ignore t :which-key "Git - Magit")
+  "gs"  '(helm-buffers-list :which-key "magit status")
   ;; Buffers
   "b" '(:ignore t :which-key "Buffers")
   "bb"  '(helm-buffers-list :which-key "buffers list")
+  "bi"  '(ibuffer :which-key "ibuffer")
+  "bd"  '(kill-this-buffer :which-key "kill buffer")
+  "bs"  '(ivy-switch-buffer :which-key "switch buffer")
+  "u" '(:ignore t :which-key "Undo")
+  "uu" '(undo-tree-visualize :which-key "Undo Tree")
    ;; Buffers
   "f" '(:ignore t :which-key "File")
   "fr"  '(ranger :which-key "open ranger")
   "fd"  '(dired :which-key "open dired")
-  "ff"  '(helm-find-files :which-key "find files")
+  "ff"  '(counsel-find-file :which-key "find files")
   ;; Window
    "w" '(:ignore t :which-key "Windows")
   "wl"  '(windmove-right :which-key "move right")
@@ -152,6 +269,7 @@
   "w/"  '(split-window-right :which-key "split right")
   "w-"  '(split-window-below :which-key "split bottom")
   "wx"  '(delete-window :which-key "delete window")
+  "wd"  '(delete-window :which-key "delete window")
   ;; Others
   "a" '(:ignore t :which-key "Applications")
   "at"  '(ansi-term :which-key "open terminal")
@@ -159,8 +277,8 @@
 ))
 
 ;; Fancy titlebar for MacOS
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
+;(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+;(add-to-list 'default-frame-alist '(ns-appearance . dark))
 (setq ns-use-proxy-icon  nil)
 (setq frame-title-format nil)
 
@@ -173,6 +291,10 @@
   (projectile-mode 1))
 ;; All The Icons
 (use-package all-the-icons :ensure t)
+
+(use-package ibuffer :ensure t)
+
+(use-package undo-tree :ensure t)
 
 ;; NeoTree
 (use-package neotree
--- init.el
+++ init.el
@@ -7,6 +7,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default)))
  '(package-selected-packages
    (quote
     (neotree projectile general which-key helm doom-themes evil-escape evil use-package))))
