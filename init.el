0eb7674e2db9176bbc21fa23c1374cd70c28a54a
bigleafio <jgraham20@gmail.com>
2018-07-17 06:56:43 -0500

    Cleanup and move custom to separate file

--- init.el
+++ init.el
@@ -1,21 +1,4 @@
 (package-initialize)
 
-;;(require 'org-install)
+(setq custom-file "~/.emacs.d/custom.el")
 (org-babel-load-file "~/.emacs.d/emacs.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-modules
-   (quote
-    (org-bbdb org-gnus org-id org-info org-protocol org-drill org-jsinfo org-habit org-irc org-mouse org-protocol org-annotate-file org-eval org-expiry org-interactive-query org-man org-collector org-panel org-screen org-toc)))
- '(package-selected-packages
-   (quote
-    (pdf-tools neotree projectile general which-key helm doom-themes evil-escape evil use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
