0c5fa9ef27e5a462b49e6f9365e3513f62b12d8d
bigleafio <jgraham20@gmail.com>
2018-07-18 08:32:58 -0500

    Add ditaa

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(display-battery-mode t))
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (ditaa . t))))
+ '(org-babel-python-command "load-env-mypython3 python")
+ '(org-confirm-babel-evaluate nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
