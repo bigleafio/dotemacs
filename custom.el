6cce5f96eccf24465a8a5b4394add6dedad14c73
bigleafio <jgraham20@gmail.com>
2018-07-19 10:39:06 -0500

    Add Icicles and Python

--- custom.el
+++ custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-babel-load-languages (quote ((emacs-lisp . t) (ditaa . t))))
- '(org-babel-python-command "load-env-mypython3 python")
- '(org-confirm-babel-evaluate nil))
+ '(package-selected-packages
+   (quote
+    (icicles yasnippet which-key use-package ujelly-theme smart-mode-line simpleclip shackle restart-emacs ranger projectile pdf-tools ox-hugo neotree interleave imenu-list highlight-indent-guides helm git-timemachine general flycheck eyebrowse exec-path-from-shell evil-smartparens evil-magit evil-escape evil-anzu diminish diff-hl delight dashboard counsel company challenger-deep-theme base16-theme avy all-the-icons-ivy alect-themes aggressive-indent ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
