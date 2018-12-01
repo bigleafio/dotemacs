47b98de996ca9dcab576e915c307ce346754a211
Jason Graham <jgraham20@gmail.com>
2018-12-01 17:17:56 -0600

    Fixes

--- install-packages.el
+++ install-packages.el
@@ -2,84 +2,87 @@
 
 (defvar my-packages
   '(delight
-diminish
-sr-speedbar
-eyebrowse
-vscode-icon
-shackle
-imenu-list
-tabbar
-evil
-evil-escape
-evil-anzu
-smart-mode-line
-rich-minority
-ivy
-counsel
-perspective
-persp-projectile
-swiper
-avy
-all-the-icons-ivy
-eyebrowse
-ivy-rich
-helm
-dired
-dired-x
-dired-sidebar
-ibuffer
-ibuffer-sidebar
-ag
-shell-pop
-restart-emacs
-smex
-simpleclip
-which-key
-general
-ace-window
-treemacs
-treemacs-evil
-treemacs-projectile
-undo-tree
-aggressive-indent
-fill-column-indicator
-company
-exec-path-from-shell
-flycheck
-magit
-evil-magit
-diff-hl
-git-gutter
-git-timemachine
-gist
-yasnippet
-projectile
-all-the-icons
-counsel-projectile
-lispy
-cider
-clojure-mode
-smartparens
-evil-smartparens
-rainbow-mode
-multiple-cursors
-elpy
-pyenv-mode
-py-autopep8
-web-mode
-markdown-mode
-imenu-list
-org
-toc-org
-helm-bibtex
-biblio
-org-ref
-rainbow-delimiters
-ox-hugo
-adaptive-wrap
-s
-org-download
-calfw)
+    s
+    diminish
+    sr-speedbar
+    eyebrowse
+    vscode-icon
+    shackle
+    imenu-list
+    tabbar
+    evil
+    evil-escape
+    evil-anzu
+    smart-mode-line
+    rich-minority
+    ivy
+    counsel
+    perspective
+    persp-projectile
+    swiper
+    avy
+    ll-the-icons-ivy
+    eyebrowse
+    ivy-rich
+    helm
+    dired
+    dired-x
+    dired-sidebar
+    ibuffer
+    ibuffer-sidebar
+    ag
+    shell-pop
+    restart-emacs
+    smex
+    simpleclip
+    which-key
+    general
+    ace-window
+    treemacs
+    treemacs-evil
+    treemacs-projectile
+    undo-tree
+    aggressive-indent
+    fill-column-indicator
+    company
+    exec-path-from-shell
+    flycheck
+    magit
+    evil-magit
+    diff-hl
+    git-gutter
+    git-timemachine
+    gist
+    yasnippet
+    projectile
+    all-the-icons
+    counsel-projectile
+    lispy
+    cider
+    clojure-mode
+    smartparens
+    evil-smartparens
+    rainbow-mode
+    multiple-cursors
+    elpy
+    pyenv-mode
+    py-autopep8
+    web-mode
+    markdown-mode
+    imenu-list
+    org
+    toc-org
+    helm-bibtex
+    biblio
+    org-ref
+    rainbow-delimiters
+    ox-hugo
+    adaptive-wrap
+    s
+    org-download
+    calfw
+    )
+    )
 
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/"))
